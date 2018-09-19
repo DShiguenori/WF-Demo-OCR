8ba7419f794ad7d4b4c530c49c13f5f8266cfe68
Danilo Uema <Danilo.uema@bnpinfo.com.br>
2018-09-18 23:18:17 -0300

    Primeiro push

---
+++ WF Demo OCR/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WF_Demo_OCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.butOCR = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.txtLocale = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // butOCR
+            // 
+            this.butOCR.Location = new System.Drawing.Point(12, 12);
+            this.butOCR.Name = "butOCR";
+            this.butOCR.Size = new System.Drawing.Size(143, 43);
+            this.butOCR.TabIndex = 0;
+            this.butOCR.Text = "OCR";
+            this.butOCR.UseVisualStyleBackColor = true;
+            this.butOCR.Click += new System.EventHandler(this.butOCR_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 61);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(482, 377);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.Text = "";
+            // 
+            // txtLocale
+            // 
+            this.txtLocale.Location = new System.Drawing.Point(500, 61);
+            this.txtLocale.Name = "txtLocale";
+            this.txtLocale.Size = new System.Drawing.Size(482, 377);
+            this.txtLocale.TabIndex = 2;
+            this.txtLocale.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 450);
+            this.Controls.Add(this.txtLocale);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.butOCR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.RichTextBox txtResult;
+        private System.Windows.Forms.Button butOCR;
+        private System.Windows.Forms.RichTextBox txtLocale;
+    }
+}
+
---
+++ WF Demo OCR/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using Google.Cloud.Vision.V1;
+
+namespace WF_Demo_OCR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butOCR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtResult.AppendText(openFileDialog.FileName);
+
+                    var client = ImageAnnotatorClient.Create();
+
+                    var image = Image.FromFile(openFileDialog.FileName);
+
+                    var response = client.DetectText(image);
+
+                    foreach (var annotation in response)
+                    {
+                        if (annotation.Description != null)
+                        {
+                            txtResult.AppendText(annotation.Description);
+
+                            foreach (var ver in annotation.BoundingPoly.Vertices)
+                            {
+                                txtResult.AppendText("\t x: " + ver.X + ", y: " + ver.Y);
+                            }
+                            txtResult.AppendText("\r\n");
+                        }
+                    }
+
+                    var response2 = client.DetectDocumentText(image);
+
+                    TextAnnotation myObject = new TextAnnotation();
+                    // Insert code to set properties and fields of the object.  
+                    XmlSerializer mySerializer = new XmlSerializer(typeof(TextAnnotation));
+
+                    using (StringWriter textWriter = new StringWriter())
+                    {
+                        mySerializer.Serialize(textWriter, response2);
+                        txtLocale.AppendText( textWriter.ToString());
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = ex.Message;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string credential_path = @"C:\Estudo\@GoogleCloud\Credential\OWTracker-9b0d94c8daf7.json";
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credential_path);
+        }
+    }
+}
